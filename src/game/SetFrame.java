b5b7834c59d446a3e94993aa23f82014cdcf2796
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2025-01-27 02:57:29 -0400

    add custom icon

--- src/game/SetFrame.java
+++ src/game/SetFrame.java
@@ -1,5 +1,8 @@
 package game;
+
 import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class SetFrame extends JFrame {
@@ -13,8 +16,24 @@ public class SetFrame extends JFrame {
         this.setResizable(false);
         this.setBackground(Color.black);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Establecer el ícono de la ventana
+        setWindowIcon();
+
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
     }
+
+    private void setWindowIcon() {
+        try {
+            // Cargar el ícono desde el archivo
+            Image icon = ImageIO.read(getClass().getResourceAsStream("/resources/icons/icon.png"));
+            this.setIconImage(icon);
+        } catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+            System.err.println("No se pudo cargar el ícono.");
+        }
+    }
 }
+
