cc52ac538a2a4893b99d8df4271215dc0ab06a0a
Jsssus <jesusrhivan@gmail.com>
2025-01-28 00:58:42 -0400

    adding players names

--- src/pojos/Score.java
+++ src/pojos/Score.java
@@ -1,10 +1,10 @@
 package pojos;
+
 import java.awt.*;
 import java.io.*;
 import java.util.Objects;
 
 public class Score extends Rectangle {
-    // Variables para el puntaje
     static int GAME_WIDTH;
     static int GAME_HEIGHT;
     public int player1;
@@ -45,15 +45,21 @@ public class Score extends Rectangle {
         g2d.setStroke(dashed);
         g2d.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
 
-
         // Margen
         int yMargin = 20;
         int yPos = 50 + yMargin;
 
-        // Dibujar el puntaje 
-        g.drawString(String.valueOf(player1 / 10) + String.valueOf(player1 % 10), (GAME_WIDTH / 2) - 85, yPos);
-        g.drawString(String.valueOf(player2 / 10) + String.valueOf(player2 % 10), (GAME_WIDTH / 2) + 20, yPos);
+        // Dibujar el puntaje con etiquetas de jugadores con diferentes tamaños de fuente
+        g.setFont(customFont.deriveFont(30f)); // Tamaño más pequeño para "Jugador 1" y "Jugador 2"
+        //g.setColor(Color.decode("#ff7d00"));
+        g.drawString("Player 1", (GAME_WIDTH / 2) - 215, 60);
+        //g.setColor(Color.decode("#eac102"));
+        g.drawString("Player 2", (GAME_WIDTH / 2) + 95, 60);
 
+        g.setFont(customFont); // Volver al tamaño original para el puntaje
+        g.setColor(Color.decode("#ff7d00"));
+        g.drawString(String.valueOf(player1 / 10) + String.valueOf(player1 % 10), (GAME_WIDTH / 2) - 85, yPos);
+        g.setColor(Color.decode("#eac102"));
+        g.drawString(String.valueOf(player2 / 10) + String.valueOf(player2 % 10), (GAME_WIDTH / 2) + 15, yPos);
     }
 }
-
